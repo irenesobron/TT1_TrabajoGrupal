893d1f444f80d986343bed2753be0ae7f2f81baf
anpalenq <anpalenq@unirioja.es>
2026-04-14 15:20:07 +0200

    ejecuta

--- src/main/java/com/ejemplo/Principal.java
+++ src/main/java/com/ejemplo/Principal.java
@@ -2,8 +2,14 @@ package com.ejemplo;
 
 import org.springframework.boot.SpringApplication;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.ejemplo", "controller"})
 public class Principal {
+
     public static void main(String[] args) {
-        //SpringApplication.run(ServidorSimuladoApplication.class, args);
+        SpringApplication.run(Principal.class, args);
     }
 }
